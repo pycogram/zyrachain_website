24f4fc095d0fb6d1915860b238f678ff05a690ed
pycogram <kaluifesinachi@yahoo.com>
2025-10-29 22:01:23 +0100

    zyrachain html structure upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,124 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <header>
+        <nav>
+        <div>zyrachain</div>
+        <span>|</span>
+        <div>
+          <ul>
+            <li>Docs</li>
+            <li>News</li>
+            <li>$Zera</li>
+          </ul>
+          <ul>
+            <li>T</li>
+            <li>X</li>
+            <li>D</li>
+          </ul>
         </div>
+        <span>|</span>
+        <div>
+          <button>
+            <p>Button</p>
+          </button>
+        </div>
+        </nav>      
+      </header>
+      <main className="main-body">
+        <div>
+          <span><h2>The automated chain network on Pi</h2></span>
+          <span>
+            <p>Zyrachain is the foundation chain of zyra ecosystem, designed to power and connect all Zyra products</p>
+            <button>
+              <p>Explore Ecosystem</p>
+            </button>
+          </span>
+        </div>
+
+        <div>
+          <h2>Vision and Mission</h2>
+          <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Explicabo sit aut alias, mollitia nisi quibusdam dolorem atque consectetur non, 
+            labore numquam, eum cumque delectus perferendis a. Officiis voluptatum facilis reiciendis.
+          </h4>
+        </div>
+
+        <div>
+          <h2>the best eco-system on pi network</h2>
+          <div>
+            <span>
+              <h4>total user</h4>
+              <h2>100,000 +</h2>
+            </span>
+            <span>
+              <h4>products</h4>
+              <h2>3 +</h2>
+            </span>
+            <span>
+              <h4>market capitalization</h4>
+              <h2>2billion +</h2>
+            </span>
+            <span>
+              <h4>wallets downloads</h4>
+              <h2>2 million +</h2>
+            </span>
+          </div>
+        </div>
+
+        <div>
+          <h2>zyrachain utilities</h2>
+          <img src="" alt="utility-img" />
+        </div>
+
+        <div>
+          <h2>meet the team behind the magic</h2>
+          <p>A dedicated team, constantly improving what we have to offer</p>
+          <div>
+            <span><img src="" alt="team-pic-1" /></span>
+            <span><img src="" alt="team-pic-2" /></span>
+            <span><img src="" alt="team-pic-3" /></span>
+            <span><img src="" alt="team-pic-4" /></span>
+            <span><img src="" alt="team-pic-5" /></span>
+            <span><img src="" alt="team-pic-6" /></span>
+          </div>
+        </div>
+
+        <div>
+          <h2>supports</h2>
+          <div>
+            <span>
+              <img src="" alt="pi-network-official-logo" />
+            </span>
+            <span>
+              <img src="" alt="provena-official-logo" />
+            </span>
+          </div>
+          <span>- -</span>
+        </div>
+
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer>
+        <div>
+          <img src="" alt="ZYRACHAIN" />
+        </div>
+        <div>
+          <img src="" alt="zyralogo" />
+          <div>
+            <span>T</span>
+            <span>X</span>
+            <span>L</span>
+            <span>G</span>
+            <span>D</span>
+            <span>M</span>
+          </div>
+          <span></span>
+          <div>
+            <p>ZYRATEAM - @ 2025. All rights reserved.</p>
+          </div>
+        </div>
       </footer>
-    </div>
+    </>
   );
 }
