29af09353fb63ca1b7263670a538b2d3a21b28b6
pycogram <kaluifesinachi@yahoo.com>
2025-11-01 22:23:32 +0100

    styled the whole page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,122 +1,214 @@
+"use client";
+import Image from "next/image";
+
+import logo1 from "../../public/logo/logo-named.png";
+import twitter from "../../public/logo/Twitter.png";
+import telegram from "../../public/logo/Telegram.png";
+import discord from "../../public/logo/Discord.png";
+import utilityPic from "../../public/pic/utility-pic.png";
+import teamPic1 from "../../public/pic/team-pic-1.png";
+import supportLogoPi from "../../public/pic/pi-network-logo.png";
+import supportLogoProvena from "../../public/pic/provena-logo.png";
+import zLogoCustom from "../../public/pic/z-logo-custom.png";
+import zLogoMain from "../../public/pic/z-logo-main.png";
+import xl from "../../public/pic/uil-tumblr-square.png";
+import tl from "../../public/pic/uil-telegram.png";
+import ll from "../../public/pic/uil-linkedin.png";
+import gl from "../../public/pic/uil-github.png";
+import ml from "../../public/pic/uil-medium-m.png";
+import bl from "../../public/pic/uil-book-alt.png";
+import { useState } from "react";
 
 export default function Home() {
+  let currentYear = new Date().getFullYear();
+  let [isMenuActive, SetIsMenuActive] =  useState(false);
+
+  let handleMenu = () => {
+    console.log(isMenuActive);
+    SetIsMenuActive(prev => !prev);
+  }
+
   return (
     <>
-      <header>
+      <header className="header-head">
         <nav>
-        <div>zyrachain</div>
-        <span>|</span>
-        <div>
-          <ul>
-            <li>Docs</li>
-            <li>News</li>
-            <li>$Zera</li>
-          </ul>
-          <ul>
-            <li>T</li>
-            <li>X</li>
-            <li>D</li>
-          </ul>
-        </div>
-        <span>|</span>
-        <div>
-          <button>
-            <p>Button</p>
-          </button>
-        </div>
-        </nav>      
-      </header>
-      <main className="main-body">
-        <div>
-          <span><h2>The automated chain network on Pi</h2></span>
-          <span>
-            <p>Zyrachain is the foundation chain of zyra ecosystem, designed to power and connect all Zyra products</p>
-            <button>
-              <p>Explore Ecosystem</p>
-            </button>
-          </span>
-        </div>
-
-        <div>
-          <h2>Vision and Mission</h2>
-          <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Explicabo sit aut alias, mollitia nisi quibusdam dolorem atque consectetur non, 
-            labore numquam, eum cumque delectus perferendis a. Officiis voluptatum facilis reiciendis.
-          </h4>
-        </div>
-
-        <div>
-          <h2>the best eco-system on pi network</h2>
           <div>
-            <span>
-              <h4>total user</h4>
-              <h2>100,000 +</h2>
-            </span>
-            <span>
-              <h4>products</h4>
-              <h2>3 +</h2>
-            </span>
-            <span>
-              <h4>market capitalization</h4>
-              <h2>2billion +</h2>
-            </span>
-            <span>
-              <h4>wallets downloads</h4>
-              <h2>2 million +</h2>
-            </span>
-          </div>
-        </div>
-
-        <div>
-          <h2>zyrachain utilities</h2>
-          <img src="" alt="utility-img" />
-        </div>
-
-        <div>
-          <h2>meet the team behind the magic</h2>
-          <p>A dedicated team, constantly improving what we have to offer</p>
-          <div>
-            <span><img src="" alt="team-pic-1" /></span>
-            <span><img src="" alt="team-pic-2" /></span>
-            <span><img src="" alt="team-pic-3" /></span>
-            <span><img src="" alt="team-pic-4" /></span>
-            <span><img src="" alt="team-pic-5" /></span>
-            <span><img src="" alt="team-pic-6" /></span>
-          </div>
-        </div>
-
-        <div>
-          <h2>supports</h2>
-          <div>
-            <span>
-              <img src="" alt="pi-network-official-logo" />
-            </span>
-            <span>
-              <img src="" alt="provena-official-logo" />
-            </span>
-          </div>
-          <span>- -</span>
-        </div>
-
-      </main>
-      <footer>
-        <div>
-          <img src="" alt="ZYRACHAIN" />
-        </div>
-        <div>
-          <img src="" alt="zyralogo" />
-          <div>
-            <span>T</span>
-            <span>X</span>
-            <span>L</span>
-            <span>G</span>
-            <span>D</span>
-            <span>M</span>
+            <Image className={"img"} src={logo1} alt="logo-text" loading="lazy" />
           </div>
           <span></span>
           <div>
-            <p>ZYRATEAM - @ 2025. All rights reserved.</p>
+            <ul>
+              <li><p>Docs</p></li>
+              <li><p>News</p></li>
+              <li><p>$Zera</p></li>
+            </ul>
+            <ul>
+              <li>
+                <Image className={"icon-x"} src={telegram} alt="telegram" loading="lazy" />
+              </li>
+              <li>
+                <Image className={"icon-x"} src={twitter} alt="twitter" loading="lazy" />
+              </li>
+              <li>
+                <Image className={"icon-x icon-x-discord"} src={discord} alt="discord" loading="lazy" />
+              </li>
+            </ul>
           </div>
+          <span></span>
+          <div>
+            <button>
+              <p>Start</p>
+            </button>
+            <div>
+              <p className={isMenuActive ? "active-menu" : "inactive-menu"} onClick={handleMenu}></p>
+            </div>
+          </div>
+        </nav>
+        <div onClick={handleMenu} className={isMenuActive ? "menu-option-open" : "menu-option-close"}>
+          <ul>
+            <li><p>Docs</p></li>
+            <li><p>News</p></li>
+            <li><p>$Zera</p></li>
+          </ul>
+        </div>      
+      </header>
+      <main className="main-body">
+        <section>
+          <div>
+            <div>
+              <h1>The automated chain network on Pi</h1>
+            </div>
+            <div>
+              <h4>Zyrachain is the foundation chain of zyra ecosystem, designed to power and connect all Zyra products.</h4>
+              <button>
+                <p>Explore</p>
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <h2>Vision and Mission</h2>
+            <h4>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, 
+              molestias totam corrupti provident sunt, repudiandae tempora iusto 
+              earum saepe porro ducimus itaque sequi eveniet fugiat quasi iste ut 
+              vitae eaque! Voluptatum soluta nostrum, non distinctio iste ratione impedit? 
+              Explicabo, quas. Tempore blanditiis quae unde rem excepturi totam, ab ipsam aliquid, 
+              libero sint accusantium iste reiciendis placeat! Ab, saepe! Repellat, earum beatae 
+              adipisci ipsa quisquam vel est molestiae dicta inventore tempore nobis repudiandae ab 
+              voluptatum dolor alias pariatur suscipit, debitis asperiores sequi deleniti eius quaerat. 
+              Officia adipisci, qui harum id molestiae eveniet. Explicabo neque saepe dicta consequatur 
+              fugiat fuga, voluptas suscipit vel minus eaque libero voluptates nostrum voluptatem 
+              repudiandae obcaecati rem molestiae repellendus eligendi consequuntur aspernatur vitae 
+              dolorem delectus. 
+            </h4>  
+          </div>
+
+          <div>
+            <h2>Eco-system</h2>
+            <div>
+              <span>
+                <h4>total user</h4>
+                <h3>100,000 +</h3>
+              </span>
+              <span>
+                <h4>products</h4>
+                <h3>3 +</h3>
+              </span>
+              <span>
+                <h4>market capitalization</h4>
+                <h3>2 billion +</h3>
+              </span>
+              <span>
+                <h4>wallets downloads</h4>
+                <h3>2 million +</h3>
+              </span>
+            </div>
+          </div>
+
+          <div>
+            <h2>zyrachain utilities</h2>
+            <Image className={"utility-pic"} src={utilityPic} alt="utility-img" loading="lazy" />
+          </div>
+
+          <div>
+            <h2>Core team</h2>
+            <p>A dedicated team, constantly improving what we have to offer.</p>
+            <div>
+              <span>
+                <Image className={"team-pic"} src={teamPic1} alt="team-pic-1" />
+                <article>
+                  <h6>TM</h6>
+                  <h5>Software Developer</h5>
+                </article>
+              </span>
+              <span>
+                <Image className={"team-pic"} src={teamPic1} alt="team-pic-2" />
+                <article>
+                  <h6>TM</h6>
+                  <h5>Software Developer</h5>
+                </article>
+              </span>
+              <span>
+                <Image className={"team-pic"} src={teamPic1} alt="team-pic-3" />
+                <article>
+                  <h6>TM</h6>
+                  <h5>Software Developer</h5>
+                </article>
+              </span>
+              <span>
+                <Image className={"team-pic"} src={teamPic1} alt="team-pic-4" />
+                <article>
+                  <h6>TM</h6>
+                  <h5>Software Developer</h5>
+                </article>
+              </span>
+              <span>
+                <Image className={"team-pic"} src={teamPic1} alt="team-pic-5" />
+                <article>
+                  <h6>TM</h6>
+                  <h5>Software Developer</h5>
+                </article>
+              </span>
+              <span>
+                <Image className={"team-pic"} src={teamPic1} alt="team-pic-6" />
+                <article>
+                  <h6>TM</h6>
+                  <h5>Software Developer</h5>
+                </article>
+              </span>
+            </div>
+          </div>
+
+          <div>
+            <h2>supports</h2>
+            <div>
+              <span>
+                <Image className={"support-logo1"} src={supportLogoPi} alt="pi-network-official-logo" />
+              </span>
+              <span>
+                <Image className={"support-logo2"} src={supportLogoProvena} alt="provena-official-logo" />
+              </span>
+            </div>
+            <p></p>
+          </div>
+        </section>
+      </main>
+      <footer className="footer-foot">
+        <div>
+          <Image className={"logo-custom"} src={zLogoCustom} alt="zyrachain" loading="lazy"/>
+        </div>
+        <div>
+          <div className="social-logo-box">
+            <span><Image className={"social-logo-1"} src={tl} alt="zyralogo" loading="lazy"/></span>
+            <span><Image className={"social-logo-1"} src={xl} alt="zyralogo" loading="lazy"/></span>
+            <span><Image className={"social-logo-1"} src={ll} alt="zyralogo" loading="lazy"/></span>
+            <span><Image className={"social-logo-1"} src={gl} alt="zyralogo" loading="lazy"/></span>
+            <span><Image className={"social-logo-1"} src={ml} alt="zyralogo" loading="lazy"/></span>
+            <span><Image className={"social-logo-1"} src={bl} alt="zyralogo" loading="lazy"/></span>
+          </div>
+          <h6></h6>
+          <p>ZYRATEAM @ {currentYear}. All rights reserved.</p>
         </div>
       </footer>
     </>
