328276c40ea71bd3214aad60a28629a2c3f8456c
pycogram <kaluifesinachi@yahoo.com>
2025-12-14 22:52:27 +0100

    implemented option to select mode and language

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ import zyrauser from "../../public/pic/zyrauser.png";
 import { motion} from "framer-motion";
 const MotionImage = motion.create(Image);
 
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, JSX, useRef } from "react";
 
 import Link from "next/link";
 
@@ -68,7 +68,7 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, WEBSITE_LOADER_TIME * 1000);
+    }, WEBSITE_LOADER_TIME * 2000);
 
     return () => clearTimeout(timer);
   }, []);
@@ -78,6 +78,8 @@ export default function Home() {
 
   const handleMenu = () => {
     console.log(isMenuActive);
+    setShowOptionLang(null);
+    setShowOptionMode(null);
     SetIsMenuActive(prev => !prev);
   }
 
@@ -108,6 +110,9 @@ export default function Home() {
   const [lastScrollY, setLastScrollY] = useState(0);
 
   const controlNavbar = useCallback(() => {
+    setShowOptionLang(null);
+    setShowOptionMode(null);
+    
     if(window.scrollY > lastScrollY && window.scrollY > 50) {
       SetIsMenuActive(false);
       setShow(false); 
@@ -121,6 +126,66 @@ export default function Home() {
     window.addEventListener("scroll", controlNavbar);
     return () => window.removeEventListener("scroll", controlNavbar);
   }, [controlNavbar]);
+
+  const [showOptionMode, setShowOptionMode] = useState<boolean | null>(false);
+  const [countOptionModeClicked, setCountOptionModeClicked] = useState<number>(0);
+
+  const modeLogoTextArray = [
+
+    {
+      name: "light mode",
+      logo: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/></svg>,
+    },
+    {
+      name: "device mode",
+      logo: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M40-120v-80h880v80H40Zm120-120q-33 0-56.5-23.5T80-320v-440q0-33 23.5-56.5T160-840h640q33 0 56.5 23.5T880-760v440q0 33-23.5 56.5T800-240H160Zm0-80h640v-440H160v440Zm0 0v-440 440Z"/></svg>,
+    },
+    {
+      name: "dark mode",
+      logo: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>,
+    }
+  ]
+
+  const [modeTL, setModeTL] = useState<any>(modeLogoTextArray[2]);
+  const hideTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  const modeBtnclick = () => {
+    setShowOptionLang(null);
+    if(hideTimeoutRef.current){
+      clearTimeout(hideTimeoutRef.current);
+    }
+    
+    if(countOptionModeClicked > 2){
+      setCountOptionModeClicked(0);
+      setShowOptionMode(null);
+      setModeTL(modeLogoTextArray[2]);
+      return;
+    }
+    
+    setCountOptionModeClicked(item => item + 1);
+    setShowOptionMode(true);
+    setModeTL(modeLogoTextArray[countOptionModeClicked]);
+
+    hideTimeoutRef.current = setTimeout(() => {
+        setShowOptionMode(false);
+      }, 5 * 1000);
+  }
+
+  const [showOptionLang, setShowOptionLang] = useState<boolean | null>(false);
+  const langBtnclick = () => {
+    setShowOptionMode(null);
+    setShowOptionLang(prev => !prev);
+  }
+
+  const listOfLang = [
+    "English", "Chinese", "Portugese", "French", "Igbo"
+  ]
+  const [optionSelected, setOptionSelected] = useState<string | null>(null);
+
+  const optionLangClicked = (lang: string) => {
+    setOptionSelected(lang)
+    langBtnclick();
+  }
 
   return (
     isLoading ?
@@ -149,7 +214,7 @@ export default function Home() {
               {
                 navContents.map((item) => (
                   <>
-                    <li onClick={handleMenu}>
+                    <li>
                       <p>{item}</p>
                     </li>
                   </>
@@ -214,10 +279,36 @@ export default function Home() {
             initial={{ x: 0 }}
             animate={{ opacity: show ? 1 : 0 }}
             transition={{ duration: 1, ease: "easeInOut" }}
-            className="lang-mode"
+            className="lang-mode-option"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 60q-74.92 0-127.46-52.54Q300-405.08 300-480q0-74.92 52.54-127.46Q405.08-660 480-660q74.92 0 127.46 52.54Q660-554.92 660-480q0 74.92-52.54 127.46Q554.92-300 480-300ZM200-450H50v-60h150v60Zm710 0H760v-60h150v60ZM450-760v-150h60v150h-60Zm0 710v-150h60v150h-60ZM262.92-656.92l-93.69-90.46 42.39-44.39 90.23 92.69-38.93 42.16Zm485.46 488.69-90.84-93.31 39.54-41.54 93.69 90.46-42.39 44.39Zm-91.46-528.85 90.46-93.69 44.39 42.39-92.69 90.23-42.16-38.93ZM168.23-211.62l93.31-90.84 40.77 39.54-90.08 94.07-44-42.77ZM480-480Z"/></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-100q-78.15 0-147.5-29.96t-120.96-81.58q-51.62-51.61-81.58-120.96T100-480q0-78.77 29.96-147.81t81.58-120.65q51.61-51.62 120.96-81.58T480-860q78.77 0 147.81 29.96t120.65 81.58q51.62 51.61 81.58 120.65T860-480q0 78.15-29.96 147.5t-81.58 120.96q-51.61 51.62-120.65 81.58T480-100Zm0-60.85q30.62-40.61 51.54-81.92 20.92-41.31 34.08-90.31H394.38q13.93 50.54 34.47 91.85 20.53 41.31 51.15 80.38Zm-77.46-11q-23-33-41.31-75.03-18.31-42.04-28.46-86.2H197.08q31.69 62.31 85 104.7 53.31 42.38 120.46 56.53Zm154.92 0q67.15-14.15 120.46-56.53 53.31-42.39 85-104.7H627.23q-12.08 44.54-30.39 86.58-18.3 42.04-39.38 74.65ZM171.92-393.08h148.7q-3.77-22.3-5.47-43.73-1.69-21.42-1.69-43.19 0-21.77 1.69-43.19 1.7-21.43 5.47-43.73h-148.7q-5.77 20.38-8.84 42.38-3.08 22-3.08 44.54t3.08 44.54q3.07 22 8.84 42.38Zm208.69 0h198.78q3.76-22.3 5.46-43.34 1.69-21.04 1.69-43.58t-1.69-43.58q-1.7-21.04-5.46-43.34H380.61q-3.76 22.3-5.46 43.34-1.69 21.04-1.69 43.58t1.69 43.58q1.7 21.04 5.46 43.34Zm258.77 0h148.7q5.77-20.38 8.84-42.38 3.08-22 3.08-44.54t-3.08-44.54q-3.07-22-8.84-42.38h-148.7q3.77 22.3 5.47 43.73 1.69 21.42 1.69 43.19 0 21.77-1.69 43.19-1.7 21.43-5.47 43.73Zm-12.15-233.84h135.69Q730.85-690 678.5-731.62q-52.35-41.61-121.04-56.92 23 34.92 40.92 76.39 17.93 41.46 28.85 85.23Zm-232.85 0h171.24q-13.93-50.16-35.04-92.43-21.12-42.27-50.58-79.8-29.46 37.53-50.58 79.8-21.11 42.27-35.04 92.43Zm-197.3 0h135.69q10.92-43.77 28.85-85.23 17.92-41.47 40.92-76.39-69.08 15.31-121.23 57.12-52.16 41.8-84.23 104.5Z"/></svg> 
+            <div className="lang-mode">
+              <span onClick={modeBtnclick}> {modeTL && modeTL.logo} </span>
+              <span onClick={langBtnclick}> 
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M480-100q-78.15 0-147.5-29.96t-120.96-81.58q-51.62-51.61-81.58-120.96T100-480q0-78.77 29.96-147.81t81.58-120.65q51.61-51.62 120.96-81.58T480-860q78.77 0 147.81 29.96t120.65 81.58q51.62 51.61 81.58 120.65T860-480q0 78.15-29.96 147.5t-81.58 120.96q-51.61 51.62-120.65 81.58T480-100Zm0-60.85q30.62-40.61 51.54-81.92 20.92-41.31 34.08-90.31H394.38q13.93 50.54 34.47 91.85 20.53 41.31 51.15 80.38Zm-77.46-11q-23-33-41.31-75.03-18.31-42.04-28.46-86.2H197.08q31.69 62.31 85 104.7 53.31 42.38 120.46 56.53Zm154.92 0q67.15-14.15 120.46-56.53 53.31-42.39 85-104.7H627.23q-12.08 44.54-30.39 86.58-18.3 42.04-39.38 74.65ZM171.92-393.08h148.7q-3.77-22.3-5.47-43.73-1.69-21.42-1.69-43.19 0-21.77 1.69-43.19 1.7-21.43 5.47-43.73h-148.7q-5.77 20.38-8.84 42.38-3.08 22-3.08 44.54t3.08 44.54q3.07 22 8.84 42.38Zm208.69 0h198.78q3.76-22.3 5.46-43.34 1.69-21.04 1.69-43.58t-1.69-43.58q-1.7-21.04-5.46-43.34H380.61q-3.76 22.3-5.46 43.34-1.69 21.04-1.69 43.58t1.69 43.58q1.7 21.04 5.46 43.34Zm258.77 0h148.7q5.77-20.38 8.84-42.38 3.08-22 3.08-44.54t-3.08-44.54q-3.07-22-8.84-42.38h-148.7q3.77 22.3 5.47 43.73 1.69 21.42 1.69 43.19 0 21.77-1.69 43.19-1.7 21.43-5.47 43.73Zm-12.15-233.84h135.69Q730.85-690 678.5-731.62q-52.35-41.61-121.04-56.92 23 34.92 40.92 76.39 17.93 41.46 28.85 85.23Zm-232.85 0h171.24q-13.93-50.16-35.04-92.43-21.12-42.27-50.58-79.8-29.46 37.53-50.58 79.8-21.11 42.27-35.04 92.43Zm-197.3 0h135.69q10.92-43.77 28.85-85.23 17.92-41.47 40.92-76.39-69.08 15.31-121.23 57.12-52.16 41.8-84.23 104.5Z"/></svg> 
+              </span>
+            </div>
+            <div className={showOptionMode ? "option-mode-show" : "option-mode-hide"}>
+                <span onClick={modeBtnclick}>
+                  {modeTL && modeTL.logo}
+                  <p>{modeTL && modeTL.name}</p>
+                </span>
+            </div>
+            <div className={showOptionLang ? "option-lang-show" : "option-lang-hide"}>
+                <span>
+                    {
+                      listOfLang.map(item => (
+                        <>
+                          <p 
+                            onClick={() => optionLangClicked(item)} 
+                            className={optionSelected && optionSelected === item ? "option-lang-selected" : "option-lang-selected-not"}>
+                              {item}
+                          </p>
+                        </>
+                      ))
+                    }
+                </span>
+            </div>
+
           </motion.div> 
 
           <MotionWrapper>
@@ -333,6 +424,13 @@ export default function Home() {
           <MotionWrapper>
             <div className="utility-part">
               <h2>zyrachain utilities</h2>
+              <p>
+                ZyraChain utilities form the foundational layer of the network, delivering essential 
+                blockchain tools and services that enable secure data exchange, efficient transaction 
+                processing, scalable application development, and transparent on-chain operations, 
+                all while supporting long-term sustainability and real-world adoption across 
+                multiple industries and use cases.
+              </p>
               <div
                 className="img-container">
                 {
@@ -361,7 +459,12 @@ export default function Home() {
           <MotionWrapper>
             <div className="core-team">
               <h2>Core team</h2>
-              <p>A dedicated team, constantly improving what we have to offer.</p>
+              <p>
+                ZyraChain is built and guided by a committed team of engineers, researchers, 
+                designers, and strategists who collaborate to develop secure, scalable, 
+                and future-ready blockchain solutions, ensuring continuous innovation, 
+                responsible network growth, and alignment with real-world blockchain needs.
+              </p>
               <div>
                 <span>
                   <Image className={"team-pic"} src={teamPic1} alt="team-pic-1" />
